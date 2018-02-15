9671d9ede33bec7ff7c83e6618bb823377a3607a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-02-15 22:12:13 +0100

    Move asset inclusion statements to their own initializer.

--- lib/ecm/translations/backend/engine.rb
+++ lib/ecm/translations/backend/engine.rb
@@ -3,11 +3,6 @@ module Ecm
     module Backend
       class Engine < ::Rails::Engine
         isolate_namespace Ecm::Translations::Backend
-
-        initializer 'ecm_translations_backend.asset_pipeline' do |app|
-          app.config.assets.precompile << 'ecm_translations_backend.js'
-          app.config.assets.precompile << 'ecm_translations_backend.css'
-        end
       end
     end
   end
