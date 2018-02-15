ebb883aaf405b97bb511bc27f779c172c3b05ba8
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-02-15 22:10:56 +0100

    Cleanup. Fix wrong params key in #permitted_params.

--- app/controllers/ecm/translations/backend/translations_controller.rb
+++ app/controllers/ecm/translations/backend/translations_controller.rb
@@ -1,34 +1,13 @@
 class Ecm::Translations::Backend::TranslationsController < Itsf::Backend::Resource::BaseController
   def self.resource_class
-    # Set the resource class here.
-    #
-    # Default: Ecm::Translations::Translation
-    #
     Ecm::Translations::Translation
   end
 
   private
 
-  def collection_scope
-    # Customize the collection scope here for collection retrival (i.e. for the
-    # index action).
-    #
-    # Example: current_user.posts.includes(:comments)
-    #
-    # Default: resource_class
-    #
-    resource_class
-  end
-
   def permitted_params
-    # Set the allowed params, for your create and update methods.
-    #
-    # Example: params
-    #            .require(:ecm_translations_translation)
-    #              .permit(:title, :body)
-    #
     params
-      .require(:ecm_translations_translation)
+      .require(:translation)
       .permit(:locale, :key, :value, :interpolations, :is_proc)
   end
 end
